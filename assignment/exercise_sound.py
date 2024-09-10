7b5e3e9b501e25f2baae314f17e778d5b0d04af4
JacobisEpic <108195485+JacobisEpic@users.noreply.github.com>
2024-09-10 11:33:13 -0400

    Update exercise_sound.py with National Anthem

--- assignment/exercise_sound.py
+++ assignment/exercise_sound.py
@@ -4,36 +4,46 @@ PWM Tone Generator
 
 based on https://www.coderdojotc.org/micropython/sound/04-play-scale/
 """
+from machine import Pin, PWM
+from utime import sleep
 
-import machine
-import utime
+buzzer = PWM(Pin(16))
 
-# GP16 is the speaker pin
-SPEAKER_PIN = 16
+tones = {
+    "B0": 31, "C1": 33, "CS1": 35, "D1": 37, "DS1": 39, "E1": 41, "F1": 44, "FS1": 46,
+    "G1": 49, "GS1": 52, "A1": 55, "AS1": 58, "B1": 62, "C2": 65,
+    "CS2": 69, "D2": 73, "DS2": 78, "E2": 82, "F2": 87, "FS2": 93, "G2": 98,
+    "GS2": 104, "A2": 110, "AS2": 117, "B2": 123, "C3": 131, "CS3": 139,
+    "D3": 147, "DS3": 156, "E3": 165, "F3": 175, "FS3": 185,
+    "G3": 196, "GS3": 208, "A3": 220, "AS3": 233, "B3": 247, "C4": 262, "CS4": 277, "D4": 294, "DS4": 311,
+    "E4": 330, "F4": 349, "FS4": 370, "G4": 392, "GS4": 415, "A4": 440, "AS4": 466, "B4": 494, "C5": 523, "CS5": 554, "D5": 587, "DS5": 622, "E5": 659, "F5": 698,
+    "FS5": 740, "G5": 784, "GS5": 831, "A5": 880, "AS5": 932, "B5": 988, "C6": 1047, "CS6": 1109, "D6": 1175, "DS6": 1245, "E6": 1319, "F6": 1397, "FS6": 1480, "G6": 1568, "GS6": 1661,
+    "A6": 1760, "AS6": 1865, "B6": 1976, "C7": 2093, "CS7": 2217, "D7": 2349, "DS7": 2489, "E7": 2637, "F7": 2794, "FS7": 2960, "G7": 3136, "GS7": 3322, "A7": 3520,
+    "AS7": 3729, "B7": 3951, "C8": 4186, "CS8": 4435, "D8": 4699, "DS8": 4978
+}
 
-# create a Pulse Width Modulation Object on this pin
-speaker = machine.PWM(machine.Pin(SPEAKER_PIN))
+national_anthem = [
+    ("G4", 0.25), ("E4", 0.25), ("C4", 0.5), ("E4", 0.5), ("G4", 0.5), ("C5", 1), ("E5", 0.25),
+    ("D5", 0.25), ("C5", 0.5), ("E4", 0.5), ("FS4", 0.5), ("G4", 1), ("G4", 0.25), ("G4", 0.25), ("G4", 0.25),
+    ("E5", 0.75), ("D5", 0.25), ("C5", 0.5), ("B4", 1), ("A4", 0.25), ("B4", 0.25), ("C5", 0.5), ("C5", 0.5),
+    ("G4", 0.5), ("E4", 0.5), ("C4", 0.5)
+]
 
+def playtone(frequency, duration):
+    buzzer.duty_u16(1000)
+    buzzer.freq(frequency)
+    sleep(duration)
 
-def playtone(frequency: float, duration: float) -> None:
-    speaker.duty_u16(1000)
-    speaker.freq(frequency)
-    utime.sleep(duration)
+def quiet(duration):
+    buzzer.duty_u16(0)
+    sleep(duration)
 
+def playsong(mysong):
+    for note, duration in mysong:
+        if note == "P":
+            quiet(duration)
+        else:
+            playtone(tones[note], duration)
+    quiet(0.1)
 
-def quiet():
-    speaker.duty_u16(0)
-
-
-freq: float = 30
-duration: float = 0.1  # seconds
-
-print("Playing frequency (Hz):")
-
-for i in range(64):
-    print(freq)
-    playtone(freq, duration)
-    freq = int(freq * 1.1)
-
-# Turn off the PWM
-quiet()
+playsong(national_anthem)
